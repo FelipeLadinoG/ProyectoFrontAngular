d4afa98642e896816a984824919ad856ad26f7cd
Omar Felipe Ladino Guezguan <oladinog@unbosque.edu.co>
2022-06-06 09:52:49 -0500

    Adding loggin without validations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './view/login/login.component';
+import {DashboardComponent} from './view/dashboard/dashboard.component';
+import {RegistroUsuarioComponent} from './view/registro-usuario/registro-usuario.component';
+import {EditarUsuarioComponent} from './view/editar-usuario/editar-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'login', pathMatch: 'full'},
+  {path: 'login', component:LoginComponent},
+  {path: 'registro-usuario', component:RegistroUsuarioComponent},
+  {path: 'editar-usuario', component:EditarUsuarioComponent},
+  {path:'dashboard', component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent,DashboardComponent,RegistroUsuarioComponent,EditarUsuarioComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './plantillas/header/header.component';
 import { FooterComponent } from './plantillas/footer/footer.component';
-import { LoginComponent } from './view/login/login.component';
-import { DashboardComponent } from './view/dashboard/dashboard.component';
-import { RegistroUsuarioComponent } from './view/registro-usuario/registro-usuario.component';
-import { EditarUsuarioComponent } from './view/editar-usuario/editar-usuario.component';
+
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent,
-    DashboardComponent,
-    RegistroUsuarioComponent,
-    EditarUsuarioComponent
+    routingComponents
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule, 
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
