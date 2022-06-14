11b6f649d3a4c06e31a262b21cb742bec2788867
Omar Felipe Ladino Guezguan <oladinog@unbosque.edu.co>
2022-06-13 23:25:27 -0500

    Editando el dashboard

--- src/app/view/editar-usuario/editar-usuario.component.ts
+++ src/app/view/editar-usuario/editar-usuario.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/servicios/api/api.service';
+import { RegisterI } from 'src/app/modelos/register.interface';
+
 
 @Component({
-  selector: 'app-editar-usuario',
+  selector: 'app-registro-usuario',
   templateUrl: './editar-usuario.component.html',
   styleUrls: ['./editar-usuario.component.css']
 })
 export class EditarUsuarioComponent implements OnInit {
+  registerForm = new FormGroup({
+    correo : new FormControl('',Validators.required),
+    contrasena : new FormControl('', Validators.required),
+    direccion : new FormControl('', Validators.required),
+    nombre : new FormControl('', Validators.required),
+    telefono : new FormControl('', Validators.required)
+  })
 
-  constructor() { }
+  constructor(private api:ApiService) { }
 
   ngOnInit(): void {
+    
+  }
+
+  onRegister(form: RegisterI){
+    this.api.register(form).subscribe(data=>{
+      console.log(data)
+    })
   }
 
 }
