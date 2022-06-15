de1771961e92631620f4377af8d971722b129d85
Omar Felipe Ladino Guezguan <oladinog@unbosque.edu.co>
2022-06-15 00:24:06 -0500

    Dashboard mejorado, eliminación de algunas opciones innecesarias

---
+++ src/app/modelos/editarUsuario.interface.ts
@@ -0,0 +1,8 @@
+export interface EditarUsuarioI{
+    id:string;
+    correo:string;
+    contrasena:string;
+    direccion:string;
+    nombre:string;
+    telefono:string;
+}
---
+++ src/app/modelos/listaUsuarios.interface.ts
@@ -0,0 +1,9 @@
+export interface ListaUsuariosI {
+    id:string;
+    contrasena:string;
+    correo:string;
+    direccion:string;
+    id_rol:string;
+    nombre:string;
+    telefono:string;
+}
